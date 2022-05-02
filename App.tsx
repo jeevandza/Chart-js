eddc1880bce708946a514d5b15b7c916d8032ff7
jeevandza <jeevandza4@gmail.com>
2022-05-02 14:18:07 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './App.css';
+import BarChart from './BarChart';
+import LineChart from './LineChart';
+import PieChart from './PieChart';
+import { UserData } from './Data';
+
+function App() {
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: 'Users Gained',
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: [
+          'rgba(75,192,192,1)',
+          '#fffff',
+          '#73D5F4',
+          '#f3ba2f',
+          '#2a71d0',
+        ],
+        borderColor: 'black',
+        borderWidth: 2,
+      },
+    ],
+  });
+
+  // IF YOU SEE THIS COMMENT: I HAVE GOOD EYESIGHT
+
+  return (
+    <div className="App">
+      <div style={{ width: 700 }}>
+        <BarChart chartData={userData} />
+      </div>
+      <div style={{ width: 700 }}>
+        <LineChart chartData={userData} />
+      </div>
+      <div style={{ width: 700 }}>
+        <PieChart chartData={userData} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
